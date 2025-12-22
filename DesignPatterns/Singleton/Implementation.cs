68be06450020ead102b73c25f367f40e2612481b
Mohamadreza <farahbakhsh20mohamadreza30@gmail.com>
2025-12-22 16:00:38 +0330

    implementation of Singleton

---
+++ DesignPatterns/Singleton/Implementation.cs
@@ -0,0 +1,55 @@
+﻿namespace Singleton
+{
+    public class Logger
+    {
+        //Lazy<T>
+        private static readonly Lazy<Logger> _lazyLogger = new Lazy<Logger>(() => new Logger());
+
+        //private static readonly Lazy<Logger> _lazyLogger = new Lazy<Logger>(CreateLogger());
+
+        //private static Logger CreateLogger()
+        //{
+        //    return new Logger();
+        //}
+
+        public static Logger Instance
+        {
+            get
+            { return _lazyLogger.Value; }
+        }
+
+        protected Logger()
+        {
+        }
+
+        public void Log(string message)
+        {
+            Console.WriteLine($"Message to log {message}");
+        }
+    }
+
+    //public class Logger
+    //{
+    //private static Logger? _instance;
+
+    //public static Logger Instance
+    //{
+    //    get
+    //    {
+    //        if (_instance == null)
+    //            _instance = new Logger();
+
+    //        return _instance;
+    //    }
+    //}
+
+    //protected Logger() 
+    //{
+    //}
+
+    //public void Log(string message)
+    //{
+    //    Console.WriteLine($"Message to log {message}");
+    //}
+    //}
+}
---
+++ DesignPatterns/Singleton/Program.cs
@@ -0,0 +1,15 @@
+﻿using Singleton;
+
+Console.Title = "Singleton Pattern";
+
+//Logger logger = new Logger();
+
+Logger instance1 = Logger.Instance;
+Logger instance2 = Logger.Instance;
+
+if (instance1 == instance2 && instance2 == Logger.Instance)
+    Console.WriteLine("Instances are the same.");
+
+instance1.Log(nameof(instance1));
+instance2.Log(nameof(instance2));
+Logger.Instance.Log(nameof(Logger.Instance));
