07ed05414434fb466f1497b371e02e0cf4191cce
Mohamadreza <farahbakhsh20mohamadreza30@gmail.com>
2025-12-24 15:33:26 +0330

    Singleton Pattern - TimCorey

---
+++ A_Creational/01_Singleton/Singleton_TableServerTimCorey/Program.cs
@@ -0,0 +1,50 @@
+﻿using Singleton_TableServerTimCorey;
+
+//TableServers tableServers = new TableServers();
+
+//for(int i = 0; i < 5; i++)
+//{
+//    Console.WriteLine("The next server is: " + tableServers.NextServer());
+//}
+
+
+
+//TableServers host1List = new TableServers();
+//TableServers host2List = new TableServers();
+
+//void Host1NextServer()
+//{
+//    Console.WriteLine("The next server is: " + host1List.NextServer());
+//}
+
+//void Host2NextServer()
+//{
+//    Console.WriteLine("The next server is: " + host2List.NextServer());
+//}
+
+//for (int i = 0; i < 5; i++)
+//{
+//    Host1NextServer();
+//    Host2NextServer();
+//}
+
+
+
+TableServers host1List = TableServers.GetTableServers();
+TableServers host2List = TableServers.GetTableServers();
+
+void Host1NextServer()
+{
+    Console.WriteLine("The next server is: " + host1List.NextServer());
+}
+
+void Host2NextServer()
+{
+    Console.WriteLine("The next server is: " + host2List.NextServer());
+}
+
+for (int i = 0; i < 5; i++)
+{
+    Host1NextServer();
+    Host2NextServer();
+}
---
+++ A_Creational/01_Singleton/Singleton_TableServerTimCorey/TableServers.cs
@@ -0,0 +1,58 @@
+﻿namespace Singleton_TableServerTimCorey
+{
+    //public class TableServers
+    //{
+    //    private List<string> Servers = new List<string>();
+    //    private int nextServer = 0;
+
+    //    public TableServers()
+    //    {
+    //        Servers.Add("Bob");
+    //        Servers.Add("Tim");
+    //        Servers.Add("Sue");
+    //        Servers.Add("Mary");
+    //    }
+
+    //    public string NextServer()
+    //    {
+    //        string next = Servers[nextServer];
+
+    //        nextServer += 1;
+    //        if (nextServer == Servers.Count)
+    //            nextServer = 0;
+    //        //nextServer = (nextServer + 1) % Servers.Count;
+
+    //        return next;
+    //    }
+    //}
+
+    public class TableServers
+    {
+        private static readonly TableServers _instance = new TableServers();
+     
+        private List<string> Servers = new List<string>();
+        private int nextServer = 0;
+
+        private TableServers()
+        {
+            Servers.Add("Bob");
+            Servers.Add("Tim");
+            Servers.Add("Sue");
+            Servers.Add("Mary");
+        }
+
+        public static TableServers GetTableServers()
+        {
+            return _instance;
+        }
+
+        public string NextServer()
+        {
+            string next = Servers[nextServer];
+
+            nextServer = (nextServer + 1) % Servers.Count;
+
+            return next;
+        }
+    }
+}
