9e8cc50fc5d12e142983e7ae78b3d107965b5e41
Mohamadreza <farahbakhsh20mohamadreza30@gmail.com>
2025-12-27 13:54:59 +0330

    AbstractFactory vs FactoryMethod

---
+++ A_Creational/03_AbstractFactory/ReportingSystem_FactoryMethodvsAbstractFactory/FactoryMethodImplementation.cs
@@ -0,0 +1,160 @@
+﻿namespace ReportingSystem_FactoryMethodvsAbstractFactory;
+
+public interface IHeader
+{
+    void RenderHeader();
+}
+
+public interface IBody
+{
+    void RenderBody();
+}
+
+public class PDFHeader : IHeader
+{
+    public void RenderHeader()
+    {
+        Console.WriteLine("PDFHeader rendering");
+    }
+}
+
+public class PDFBody : IBody
+{
+    public void RenderBody()
+    {
+        Console.WriteLine("PDFBody rendering");
+    }
+}
+
+public class ExcelHeader : IHeader
+{
+    public void RenderHeader()
+    {
+        Console.WriteLine("ExcelHeader rendering");
+    }
+}
+
+public class ExcelBody : IBody
+{
+    public void RenderBody()
+    {
+        Console.WriteLine("ExcelBody rendering");
+    }
+}
+
+public abstract class HeaderCreator
+{
+    public abstract IHeader CreateHeader();
+}
+
+public abstract class BodyCreator
+{
+    public abstract IBody CreateBody();
+}
+
+public class PDFHeaderCreator : HeaderCreator
+{
+    public override IHeader CreateHeader()
+    {
+        return new PDFHeader();
+    }
+}
+
+public class ExcelHeaderCreator : HeaderCreator
+{
+    public override IHeader CreateHeader()
+    {
+        return new ExcelHeader();
+    }
+}
+
+public class PdfBodyCreator : BodyCreator
+{
+    public override IBody CreateBody()
+    {
+        return new PDFBody();
+    }
+}
+
+public class ExcelBodyCreator : BodyCreator
+{
+    public override IBody CreateBody()
+    {
+        return new ExcelBody();
+    }
+}
+
+//------------------------------------------------------------
+public abstract class ReportCreator
+{
+    public abstract IHeader CreateHeader();
+    public abstract IBody CreateBody();
+
+    public void CreateReport()
+    {
+        IHeader header = CreateHeader();
+        IBody body = CreateBody();
+
+        header.RenderHeader();
+        body.RenderBody();
+    }
+}
+
+public class PDFReportCreator : ReportCreator
+{
+    public override IHeader CreateHeader()
+    {
+        return new PDFHeader();
+    }
+
+    public override IBody CreateBody()
+    {
+        return new PDFBody();
+    }
+}
+
+public class ExcelReportCreator : ReportCreator
+{
+    public override IHeader CreateHeader()
+    {
+        return new ExcelHeader();
+    }
+
+    public override IBody CreateBody()
+    {
+        return new ExcelBody();
+    }
+}
+
+//------------------------------------------------------------
+public interface IReportFactory
+{
+    IHeader CreateHeader();
+    IBody CreateBody();
+}
+
+public class PdfReportFactory : IReportFactory
+{
+    public IHeader CreateHeader()
+    {
+        return new PDFHeader();
+    }
+
+    public IBody CreateBody()
+    {
+        return new PDFBody();
+    }
+}
+
+public class ExcelReportFactory : IReportFactory
+{
+    public IHeader CreateHeader()
+    {
+        return new ExcelHeader();
+    }
+
+    public IBody CreateBody()
+    {
+        return new ExcelBody();
+    }
+}
---
+++ A_Creational/03_AbstractFactory/ReportingSystem_FactoryMethodvsAbstractFactory/Program.cs
@@ -0,0 +1,26 @@
+﻿using ReportingSystem_FactoryMethodvsAbstractFactory;
+
+HeaderCreator headerFactory = new ExcelHeaderCreator();
+BodyCreator bodyFactory = new PdfBodyCreator();
+
+IHeader header = headerFactory.CreateHeader();
+IBody body = bodyFactory.CreateBody();
+
+header.RenderHeader();
+body.RenderBody();
+Console.WriteLine();
+
+//------------------------------------------------------------
+PDFReportCreator reportCreator = new PDFReportCreator();
+reportCreator.CreateReport();
+Console.WriteLine();
+
+//------------------------------------------------------------
+IReportFactory factory = new PdfReportFactory(); 
+
+IHeader header2 = factory.CreateHeader();
+IBody body2 = factory.CreateBody();
+
+header2.RenderHeader();
+body2.RenderBody();
+Console.WriteLine();
