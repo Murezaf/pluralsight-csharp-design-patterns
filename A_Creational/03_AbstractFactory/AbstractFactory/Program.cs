c1c54554b1bf2f2f60d6fd0c92effb22cf5c7196
Mohamadreza <farahbakhsh20mohamadreza30@gmail.com>
2025-12-27 11:32:37 +0330

    Abstract Factory

---
+++ A_Creational/03_AbstractFactory/AbstractFactory/Program.cs
@@ -0,0 +1,13 @@
+﻿using static AbstractFactory.Implementation;
+
+Console.Title = "Abstract Factory";
+
+BelgiumShoppingCartPurchaseFactory belgiumShoppingCartPurchaseFactory = new BelgiumShoppingCartPurchaseFactory();
+ShoppingCart shoppingCartForBelgium = new ShoppingCart(belgiumShoppingCartPurchaseFactory);
+
+shoppingCartForBelgium.CalculateCosts();
+
+FranceShoppingCartPurchaseFactory franceShoppingCartPurchaseFactory = new FranceShoppingCartPurchaseFactory();
+ShoppingCart shoppingCartForFrance = new ShoppingCart(franceShoppingCartPurchaseFactory);
+
+shoppingCartForFrance.CalculateCosts();
