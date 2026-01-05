84a17b8123d54198360489ec2e4b0f02b41b47b5
Mohamadreza <farahbakhsh20mohamadreza30@gmail.com>
2026-01-05 23:06:39 +0330

    Adapter_ThirdParty

---
+++ B_Structural/01_Adapter/Adapter_ThirdParty/Implementation.cs
@@ -0,0 +1,46 @@
+﻿namespace Adapter_ThirdParty;
+
+//Third‑Party Library (Adaptee)
+public class ExternalMailSdk
+{
+    public void SendPlain(string mail, string text) { }
+    public void SendHtml(string mail, string html) { }
+}
+
+//helper
+public class EmailMessage
+{
+    public string To;
+    public string Subject;
+    public string Body;
+    public bool IsHtml;
+}
+
+//Target
+public interface IEmailService
+{
+    void Send(EmailMessage message);
+}
+
+//Adapter
+public class MailAdapter : IEmailService
+{
+    private readonly ExternalMailSdk _sdk;
+
+    public MailAdapter(ExternalMailSdk sdk)
+    {
+        _sdk = sdk;
+    }
+
+    public void Send(EmailMessage message)
+    {
+        Console.WriteLine("[Adapter] Mapping EmailMessage to ExternalMailMessage");
+
+        if (message.IsHtml)
+            _sdk.SendHtml(message.To, message.Body);
+        else
+            _sdk.SendPlain(message.To, message.Body);
+
+        Console.WriteLine("[Adapter] Mail forwarded to ExternalMailSdk successfully");
+    }
+}
---
+++ B_Structural/01_Adapter/Adapter_ThirdParty/Program.cs
@@ -0,0 +1,21 @@
+﻿using Adapter_ThirdParty;
+
+var externalSdk = new ExternalMailSdk();
+
+IEmailService emailService = new MailAdapter(externalSdk);
+
+var message = new EmailMessage
+{
+    To = "test@example.com",
+    Subject = "Hello",
+    Body = "<h1>Hello Adapter</h1>",
+    IsHtml = true
+};
+
+emailService.Send(message);
+
+//DI Container:
+//var builder = WebApplication.CreateBuilder(args);
+//builder.Services.AddScoped<ExternalMailSdk>();
+//builder.Services.AddScoped<IEmailService, MailAdapter>();
+//var app = builder.Build();
